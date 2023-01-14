7f09b338bbfbb6456481a10a1f5e2c8a86ee0500
yyubin <hazing120@gmail.com>
2023-01-15 06:59:35 +0900

    first_go_commit

---
+++ main_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUploadTest(t *testing.T) {
+	assert := assert.New(t)
+	path := "/Users/yubin/Documents/golang/src/goweb2/uploads/1612312679152－2.jpg"
+	file, _ := os.Open(path)
+	defer file.Close()
+
+	os.RemoveAll("./uploads")
+
+	buf := &bytes.Buffer{}
+	writer := multipart.NewWriter(buf)
+	multi, err := writer.CreateFormFile("upload_file", filepath.Base(path))
+	assert.NoError(err)
+
+	io.Copy(multi, file)
+	writer.Close()
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("POST", "/uploads", buf)
+	req.Header.Set("Content-type", writer.FormDataContentType())
+
+	uploadHandler(res, req)
+	assert.Equal(http.StatusOK, res.Code)
+
+	uploadFilePath := "./uploads/" + filepath.Base(path)
+	_, err1 := os.Stat(uploadFilePath)
+	assert.NoError(err1)
+
+	uploadFile, _ := os.Open(uploadFilePath)
+	originFile, _ := os.Open(path)
+	defer uploadFile.Close()
+	defer originFile.Close()
+
+	uploadData := []byte{}
+	originData := []byte{}
+	uploadFile.Read(uploadData)
+	originFile.Read(originData)
+
+	assert.Equal(originData, uploadData)
+
+}
